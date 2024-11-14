41da14359ba5327401ed22090a9fb3bd12a36d97
MxmTTV <mxmttv@gmail.com>
2024-11-14 22:46:55 +0300

    result

--- main.go
+++ main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
+
+// Структура для декодирования JSON
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+// Глобальная переменная для хранения сообщения
+var task string
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/hello", Handler).Methods("GET")
+	router.HandleFunc("/get", handlerGet).Methods("GET")
+	router.HandleFunc("/api/hello", handlerPost).Methods("POST")
+
+	// посылаю ПОСТ-запрос
+	http.Post("http://localhost:8080/api/hello", "application/json", strings.NewReader(`{"message": "World"}`))
+
 	http.ListenAndServe(":8080", router)
+
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// читаю тело запроса
+	var body requestBody
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&body)
+	if err != nil {
+		http.Error(w, "Ошибка при декодировании JSON", http.StatusBadRequest)
+		return
+	}
+
+	// присвоил значение глобальной переменной
+	task = body.Message
+
+}
+
+func handlerGet(w http.ResponseWriter, r *http.Request) {
+	// возвращаю текущее значение глобальной переменной
+	fmt.Fprintf(w, "Hello, %s\n", task)
 }
